2788d7cd95247926b63b751599ac2fe9c25da0ff
luisjoseflores <ljfloresr@gmail.com>
2026-06-27 03:46:54 -0500

    subir

--- contracts/21_BallenitaNFT.sol
+++ contracts/21_BallenitaNFT.sol
@@ -20,22 +20,54 @@ contract BallenitaNFT is ERC721, Ownable {
         string nombre;
         uint256 level;
         uint256 rareza;
+        uint256 precio;
     }
 
     mapping(uint256 => Poster) private posters;
 
-    function mint(address to, string memory _nombre, uint256 _level, uint256 _rareza) public onlyOwner {
+    function mint(string memory _nombre, uint256 _level, uint256 _rareza) public onlyOwner {
 
         uint idPoster = _contadorId;
-        posters[idPoster] = Poster(idPoster, _nombre, _level, _rareza);
-        _safeMint(to, _contadorId);
+
+        uint256 precioCalculado = 0.001 ether * _level * _rareza;
+
+        posters[idPoster] = Poster(idPoster, _nombre, _level, _rareza, precioCalculado);
+        _mint(address(this), _contadorId);
         _contadorId++;
 
     }
 
-    function verPoster(uint256 tokenId) public view returns (uint256 id, string memory nombre, uint256 level, uint256 rareza) {
+    function verPoster(uint256 tokenId) public view returns (uint256 id, string memory nombre, uint256 level, uint256 rareza, uint256 precio) {
         Poster memory poster = posters[tokenId];
-        return (poster.id, poster.nombre, poster.level, poster.rareza);
+        return (poster.id, poster.nombre, poster.level, poster.rareza, poster.precio);
     }
+
+    function verPostersProp(address propietario) public view returns(Poster[] memory){
+        uint256 cantidadPosters = balanceOf(propietario);
+        Poster[] memory resultado = new Poster[](cantidadPosters);
+        
+        uint256 contador = 0;
+
+        for(uint256 tokenId = 1; tokenId < _contadorId; tokenId++){
+            if(_ownerOf(tokenId) == propietario) {
+                resultado[contador] = posters[tokenId];
+                contador++;
+            }
+        }
+
+        return resultado;
+    }
+
+    function comprarPoster(uint256 tokenId) public payable {
+        require(ownerOf(tokenId) == address(this), "El poster no esta disponible");
+        Poster memory poster = posters[tokenId];
+        require(msg.value >= poster.precio, "ETH insuficiente");        
+        _transfer(address(this), msg.sender, tokenId);
+    }
+
+    function retirar() public onlyOwner {
+        payable(owner()).transfer(address(this).balance);
+    } 
+
 
 }
